a7f1cc75555915d7bc037235ffca2e4d476b06e6
Michal Podrouzek <michallo64@gmail.com>
2021-03-08 11:42:36 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Michal Podroužek on 08/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
